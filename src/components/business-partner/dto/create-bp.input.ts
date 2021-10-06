1cb61a72caeda410c468c5f13aa16fce599aa647
abuahmed <ibrayas@gmail.com>
2021-10-06 15:01:29 +0300

    added credit fields to bp input type

--- src/components/business-partner/dto/create-bp.input.ts
+++ src/components/business-partner/dto/create-bp.input.ts
@@ -1,4 +1,4 @@
-import { InputType, PartialType } from '@nestjs/graphql';
+import { Field, InputType, Int, PartialType } from '@nestjs/graphql';
 import { AddressInput } from 'src/components/dto/address.input';
 import { ContactInput } from 'src/components/dto/contact.input';
 import { DisplayInput } from 'src/components/dto/display.input';
@@ -7,6 +7,10 @@ import { BusinessPartnerType } from 'src/db/enums/businessPartnerType';
 @InputType()
 export class CreateBusinessPartnerInput extends PartialType(DisplayInput) {
   type: BusinessPartnerType;
+  initialOutstandingCredit?: number;
+  creditLimit?: number;
+  @Field(() => Int, { defaultValue: 0 })
+  creditTransactionsLimit?: number;
   address: AddressInput;
   contact: ContactInput;
 }
