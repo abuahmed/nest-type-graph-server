bb50cc27a1288cdca70cca9835040ccd31fc92d5
abuahmed <ibrayas@gmail.com>
2021-10-13 18:57:47 +0300

    added transfer roles

--- src/data/roles.ts
+++ src/data/roles.ts
@@ -1,70 +1,83 @@
 export default [
-  //'Admin',
-  'View Dashboard',
-
-  //   'Client Profile',
-  //   'Organization Profile',
-  //   'Add Stores',
-
-  //   'Settings',
-  //   'Advanced Settings',
-  //   'Tax Settings',
-  //   'General Settings',
-
-  //   'Update Stores',
-  //   //'Add/Edit Bank Account',
-  'Users Management',
-  //'Users Privilege Management',
-
-  'OnHand Inventory',
-  //'Reserve Items',
-  //'Items Quantity',
-
-  'Physical Inventory',
-  'View Physical Inventory',
-  'Add Physical Inventory',
-  'Post Physical Inventory',
-  'UnPost Physical Inventory',
-  'Delete Physical Inventory',
-  'Pi Lines History',
-
-  'Customers Entry',
-  //'Customers Advanced',
-  'Vendors Entry',
-
-  'Items Entry',
-  //   'Item Categories',
-  //   'Item Unit Of Measures',
-
-  'Sales',
-  'View Sales',
-  'Add Sales',
-  'Post Sales',
-  'UnPost Sales',
-  'Delete Sales',
-  'Sales Lines History',
-  //   'Sales Payments',
-
-  'Purchase',
-  'View Purchase',
-  'Add Purchase',
-  'Post Purchase',
-  'UnPost Purchase',
-  'Delete Purchase',
-  'Purchase Lines History',
-  //   'Purchase Payments',
-
-  //   'Payments',
-  //   'Add Checks',
-  //   'Deposit Payments',
-  //   'Clear Payments',
-  //   'Expenses and Loans',
-
-  //   'Cpo',
-  //   'Item Borrows',
-
-  //   'View Reports',
+  'View Transfer',
+  'Add Transfer',
+  'Post Transfer',
+  'UnPost Transfer',
+  'Delete Transfer',
 ];
+// export default [
+//   //'Admin',
+//   'View Dashboard',
+
+//   //   'Client Profile',
+//   //   'Organization Profile',
+//   //   'Add Stores',
+
+//   //   'Settings',
+//   //   'Advanced Settings',
+//   //   'Tax Settings',
+//   //   'General Settings',
+
+//   //   'Update Stores',
+//   //   //'Add/Edit Bank Account',
+//   'Users Management',
+//   //'Users Privilege Management',
+
+//   'OnHand Inventory',
+//   //'Reserve Items',
+//   //'Items Quantity',
+
+//   'Physical Inventory',
+//   'View Physical Inventory',
+//   'Add Physical Inventory',
+//   'Post Physical Inventory',
+//   'UnPost Physical Inventory',
+//   'Delete Physical Inventory',
+//   'Pi Lines History',
+
+//   'Customers Entry',
+//   //'Customers Advanced',
+//   'Vendors Entry',
+
+//   'Items Entry',
+//   //   'Item Categories',
+//   //   'Item Unit Of Measures',
+
+//   'Sales',
+//   'View Sales',
+//   'Add Sales',
+//   'Post Sales',
+//   'UnPost Sales',
+//   'Delete Sales',
+//   'Sales Lines History',
+//   //   'Sales Payments',
+
+//   'Purchase',
+//   'View Purchase',
+//   'Add Purchase',
+//   'Post Purchase',
+//   'UnPost Purchase',
+//   'Delete Purchase',
+//   'Purchase Lines History',
+//   //   'Purchase Payments',
+
+//   //   'Payments',
+//   //   'Add Checks',
+//   //   'Deposit Payments',
+//   //   'Clear Payments',
+//   //   'Expenses and Loans',
+
+//   //   'Cpo',
+//   //   'Item Borrows',
+
+//   //   'View Reports',
+
+// 'View Transfer',
+//   'Add Transfer',
+//   'Post Transfer',
+//   'UnPost Transfer',
+//   'Delete Transfer',
+// ];
 
 export enum Role {
   ViewDashboard = 'View Dashboard',
